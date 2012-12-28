abced8c34b364b5bee8cbfef562dd54ef6bb0e70
Lawrence Siebert <gryftir@gmail.com>
2012-12-27 20:32:25 -0800

    added readme

--- euler1-1.pl
+++ euler1-1.pl
@@ -10,7 +10,11 @@ print "result: ", sumbydiv(3, $highval) + sumbydiv(5, $highval) - sumbydiv(15, $
 
 sub sumbydiv  #sum of multiples of first val
 {		
+	#you can return the sum of natural numbers from 1 to n as n(n+1)/2
+	#therefore you can return the sum of all natural numbebers from 1 to n divisible by x
+	#as n div x = p, x * p * (p+1)/2 we subtract 1 from n because we are looking for numbers below the specified n 	
 	my $p = int(($_[1] - 1) / $_[0]); 
 	return ($_[0] * $p * ($p + 1))/2; 
 }
+#run with perl euler1-1.perl 1000 to get the correct answer
 
