c6e71f958aa3e94c15e6f2482b7ffd6a2899d17b
Lawrence Siebert <gryftir@gmail.com>
2012-12-28 02:03:13 -0800

    somewhat optimized approach, think this works

--- euler3.pl
+++ euler3.pl
@@ -3,5 +3,36 @@ use warnings;
 use strict;
 @ARGV == 1 or die "include a number to calculate to";
 my $highval = shift;
-my $root = sqrt $highval;
+my $max = 5;
+my $n = $highval;
+my @addsum = (0,2,4);
+my $addswitch = 1;
+while ($n % 2 == 0)
+{
+	$n /= 2;
+	$max = 2; 
+}
+while ($n % 3 == 0)
+{
+	$n /= 3;
+	$max = 3; 
+}
+if($n == $max)
+{
+	print $max,"\n";
+	exit 0;
+}
+$max = 5;
+while($n > $max)
+{	
+while($n % $max == 0)
+	{
+		last if($n == $max);	
+		$n /= $max;
+	}
+	last if($n == $max);
+	$max += $addsum[$addswitch];
+	$addswitch *= -1;
+}
+print $max,"\n";
 
